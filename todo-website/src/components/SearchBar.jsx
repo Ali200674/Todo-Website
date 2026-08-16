c95cd5656ab0d0c7e44e22e885f7db9d67ae6ea8
Ali Izoyev <ali200674@hotmail.com>
2026-08-16 15:32:16 -0700

    Added some comments

--- todo-website/src/components/SearchBar.jsx
+++ todo-website/src/components/SearchBar.jsx
@@ -5,11 +5,13 @@ function SearchBar() {
   return (
     <>
       <div className="search-bar-div">
+        {/* A div for the input field as the search bar */}
         <div className="search-bar">
           <input type="text" placeholder="Search for task..." />
         </div>
+        {/* Buttons near the input field */}
         <button className="filters-task">Filters</button>
-        <button className="create-task">Create Task</button>
+        <button className="create-task">+ Create Task</button>
       </div>
     </>
   );
