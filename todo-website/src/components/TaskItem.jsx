9d22b9316a877af3d7f735cb69bce779f783d500
Ali Izoyev <ali200674@hotmail.com>
2026-08-21 13:52:02 -0700

    Change ssome colors on the website.

--- todo-website/src/components/TaskItem.jsx
+++ todo-website/src/components/TaskItem.jsx
@@ -4,15 +4,21 @@ import { useState } from "react";
 
 function TaskItem({ taskName }) {
   const [isClicked, setIsClicked] = useState(false);
+  const [taskCompleted, setTaskCompleted] = useState(false);
 
   return (
     <div className="task-item-div">
       <div className="left-side-task">
         <div className="task-item-heading">
-          <h2>{taskName.taskName}</h2>
+          <h2 className={taskCompleted === true ? "cross-out-text" : ""}>
+            {taskName.taskName}
+          </h2>
         </div>
         <div className="task-check-off">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onClick={() => setTaskCompleted(!taskCompleted)}
+          />
         </div>
       </div>
       <div className="right-side">
