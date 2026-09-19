43784a6d13bda7750dcf6e364268c4461c3ef5d9
Ali Izoyev <ali200674@hotmail.com>
2026-09-18 21:45:33 -0700

    search bar function now requests data from backend and displays the results

--- todo-website/src/components/SearchBar.jsx
+++ todo-website/src/components/SearchBar.jsx
@@ -1,26 +1,52 @@
 import { useRef } from "react";
 
+import searchlogo from "../assets/search-bar-logo.svg";
+
 /**
  * This component is designed to be a search bar or filter for the user's tasks.
  *
  * @param {Function} setSearchFilter A function that sets the search filter.
  * @returns {React.ReactElement} A component to represent a search bar.
  */
-function SearchBar({ setSearchFilter }) {
+function SearchBar({ setSearchFilter, addTask }) {
   const userInput = useRef(null);
 
   function setTaskFilter() {
     setSearchFilter(userInput.current.value);
   }
 
+  async function getAllTasksBasedOnSearch(event) {
+    event.preventDefault();
+
+    const searchParam = new URLSearchParams({
+      search: userInput.current.value,
+    });
+
+    // A GET request to get all tasks from database
+    const response = await fetch(
+      `http://localhost:8080/api/tasks?${searchParam}`,
+    );
+
+    const data = await response.json();
+
+    addTask(data);
+  }
+
   return (
     <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search for task..."
-        ref={userInput}
-        onChange={setTaskFilter}
+      <img
+        src={searchlogo}
+        alt="A magnifying glass"
+        onClick={getAllTasksBasedOnSearch}
       />
+      <form action="" onSubmit={getAllTasksBasedOnSearch}>
+        <input
+          type="text"
+          placeholder="Search for task..."
+          ref={userInput}
+          onChange={setTaskFilter}
+        />
+      </form>
     </div>
   );
 }
